62ce854a6a62c2831acd164a4caca513762b8973
Corbin McDougal <mcdoucor000@warren.k12.in.us>
2024-10-23 08:11:48 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+age = int(input("Age: "))
+
+if age < 5:
+    print("You should play with toys.")
+elif age >= 5 and age <= 12:
+    print("You can play outdoors.")
+elif age := 13 and age <= 17:
+    print("You should play video games.")
+else:
+    age >= 18
+    print("Take a Hike")
